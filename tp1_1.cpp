a517cff63c0cb318d6f46d3f472c276a5cb66c46
Santiago <santiago991007@gmail.com>
2024-03-29 23:54:55 -0300

    se realizo el punto e

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    printf("hola mundo");
+    int num = 10;
+    int *puntero = &num;
+
+    printf("El contenido del puntero es: %d", *puntero);
+    printf("\nLa direccion de memoria almacenada en el puntero es: %d", &puntero);
+    printf("\nLa direccion de memoria de la variable es: %d", &num);
+    printf("\nLa direccion de memoria del puntero es: %d", &puntero);
+    printf("\n4. %d", puntero);
+    printf("\nEl tamaño de memoria utilizado es: %d", sizeof(*puntero));
     return 0;
 }
